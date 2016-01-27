f9102b4f8862aa3d9bf6301092560372650f9a6b
sbfnk <sbfnk@users.noreply.github.com>
2016-01-27 15:51:55 +0000

    loop over files with given treatment frequency

--- yaws_model_generate_results_file.R
+++ yaws_model_generate_results_file.R
@@ -1,48 +1,48 @@
 library(data.table)
 
-# get list of files in data directory
 if (Sys.info()[["user"]] == "seb") {
     data_dir <- path.expand("~/Data/Yaws/") # Seb
 } else {
     data_dir <- path.expand("~/Documents/Modelling Data/") # Michael
 }
 
-files <- list.files(path.expand("/"), pattern = ".*\\.rds", full.names = TRUE)
-
-filenames<- as.data.table(files)
-
-integers <- as.data.table(apply(filenames, 1, function(x) as.integer(substr(x, 47, 48)))) ##character reference will need adjusting based on local file path
-
-nb_events1 <- integers  %/% 6 + 1 ## divide index by 6, keep integer part, add 1
-nb_events2 <- integers %% 6 ## divide index by 6, keep modulus 
-
-mdatimes <- cbind(nb_events1,nb_events2)
-colnames(mdatimes) <- c("Rounds of TCT","Rounds of TTT")
-
-dt <- NULL
-
-system.time(for (file in files[1:X])
+for (months in c(6, 12))
 {
-  l <- readRDS(file)
-  runs <- data.table(t(sapply(l, function(x) { x[["params"]] } )))
-  
-  runs[, extinct := sapply(l, function(x) {
-    last.state <- tail(x[["trajectory"]], 1)
-    return(ifelse(last.state[, I1 + I2 + L] == 0, 1, 0))
-  })]
-  par <- t(sapply(runs, function(x) { tail(x, n = 1) }))
-  extinctcount <- t(sum(runs[["extinct"]]))
-  colnames(extinctcount) <- c("extinctcount")
-  runsum_temp <- merge (extinctcount,par)
-  runsum_temp <- runsum_temp[,-(6:11),drop=FALSE] 
+    data_dir_months <- paste0(data_dir, "/", months, "MonthFreq")
 
-    if (is.null(dt))
-  {
-    dt <- copy(runsum_temp)
-  } else
-  {
-    dt <- rbind(dt, runsum_temp)
-  }
-})
+    ## get list of files in data directory
+    files <- list.files(data_dir_months, pattern = ".*\\.rds", full.names = TRUE)
 
-totaldata <- cbind(dt,mdatimes) 
+    pb <- txtProgressBar(min = 0, max = length(files), style = 3)
+
+    system.time(for (file_idx in seq_along(files))
+                {
+                    l <- readRDS(paste(path, files[file_idx], sep = "/"))
+                    runs <- data.table(t(sapply(l, function(x) { x[["params"]] } )))
+                    runs[, extinct := sapply(l, function(x) {
+                               last.state <- tail(x[["trajectory"]], 1)
+                               return(ifelse(last.state[, I1 + I2 + L] == 0, 1, 0))
+                           })]
+
+                    integer <- sapply(files[file_idx], function(x) as.integer(substr(x, 10, 11)))
+                    nb_events1 <- integer %/% 6 + 1 ## divide index by 6, keep integer part, add 1
+                    nb_events2 <- integer %% 6 ## divide index by 6, keep modulus
+
+                    mdatimes <- data.table(cbind(nb_events1, nb_events2))
+                    setnames(mdatimes, c("Rounds of TCT","Rounds of TTT"))
+                    runs <- cbind(runs, mdatimes)
+
+                    if (is.null(dt))
+                    {
+                        dt <- copy(runs)
+                    } else
+                    {
+                        dt <- rbind(dt, runs)
+                    }
+                    setTxtProgressBar(pb, file_idx)
+                })
+
+    close(pb)
+
+    saveRDS(dt, paste0("yaws_res_", months, ".rds"))
+}
